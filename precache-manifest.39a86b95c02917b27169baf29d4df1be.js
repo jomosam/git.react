99498d2d3c0b653835dbfeb2792f5d5dad994531
jomosam@gmail.com <jomosam@gmail.com>
2019-09-26 13:30:02 +0530

    Updates

---
+++ precache-manifest.39a86b95c02917b27169baf29d4df1be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96f479771315bd46198c1a1d4def82cf",
+    "url": "/jomosam/git.react/index.html"
+  },
+  {
+    "revision": "7d79edd89092343fcc42",
+    "url": "/jomosam/git.react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1979004dbb49187fcb90",
+    "url": "/jomosam/git.react/static/js/2.b7617c08.chunk.js"
+  },
+  {
+    "revision": "7d79edd89092343fcc42",
+    "url": "/jomosam/git.react/static/js/main.7858c86e.chunk.js"
+  },
+  {
+    "revision": "5417b7be53d00ea2f330",
+    "url": "/jomosam/git.react/static/js/runtime-main.29ea8a9c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/jomosam/git.react/static/media/logo.25bf045c.svg"
+  }
+]);
